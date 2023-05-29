1c78c42bce97c97938e77bc961eccce8226e209a
ragss8 <96374035+ragss8@users.noreply.github.com>
2023-05-29 14:39:46 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,15 +1,15 @@
  function addTask() {
             var taskInput = document.getElementById('taskInput').value;
-        
+
             if (taskInput === '') {
                 alert('Please enter a task!');
                 return;
             }
-        
-            var taskList = document.getElementById('taskDisplay'); //creates a new variable tasklist and the tasks displayed from class="taskDisplay" is encapsulated here
-            var taskItem = document.createElement('li'); // creates a list of tasks and eachItem is created seperately
-            
-            var checkbox = document.createElement('input'); // new variable checkbox is created and the checkbox is taken as input to show if the task is completed or not
+
+            var taskList = document.getElementById('taskDisplay');
+            var taskItem = document.createElement('li');
+
+            var checkbox = document.createElement('input');
             checkbox.type = 'checkbox';
             checkbox.addEventListener('change', function() {
                 if (this.checked) {
@@ -18,35 +18,108 @@
                     taskItem.classList.remove('completed');
                 }
             });
-        
-            var label = document.createElement('label'); //creates a new task item with a checkbox and a label containing the task name
-            label.appendChild(checkbox);                 //The task item is then added to the task list then it clears the input field, ready for the next task to be entered.
+
+            var label = document.createElement('label');
+            label.appendChild(checkbox);
             label.appendChild(document.createTextNode(taskInput));
-            
-            //block of code that deletes a task using "X" symbol
-            var deleteSymbol = document.createElement('span');
-            deleteSymbol.textContent = 'X';
-            deleteSymbol.className = 'deleteSymbol';
-            deleteSymbol.addEventListener('click', function() {
-                deleteTaskItem(taskItem);
+
+            // Add class for hover effect
+            label.classList.add('hoverEffect');
+
+            // Add event listeners for hover events
+            label.addEventListener('mouseenter', function() {
+                showOptions(label);
             });
-            label.appendChild(deleteSymbol);
-        
+
+            label.addEventListener('mouseleave', function() {
+                hideOptions(label);
+            });
+
+            // Create delete button
+            var deleteButton = document.createElement('span');
+            deleteButton.textContent = 'X';
+            deleteButton.className = 'deleteButton';
+            deleteButton.addEventListener('click', function(event) {
+                event.stopPropagation(); // Prevents triggering the parent label's click event
+                deleteTask(taskItem);
+            });
+            label.appendChild(deleteButton);
+
             taskItem.appendChild(label);
             taskList.appendChild(taskItem);
-        
+
             document.getElementById('taskInput').value = '';
         }
 
-        function deleteTaskItem(taskItem){
+        function deleteTask(taskItem) {
             taskItem.remove();
         }
-        
+
+        function editTask(label) {
+            var currentText = label.textContent;
+
+            // Remove delete button from the label
+            var deleteButton = label.lastElementChild;
+            deleteButton.remove();
+
+            // Create an input field for editing
+            var editInput = document.createElement('input');
+            editInput.type = 'text';
+            editInput.value = currentText;
+            editInput.addEventListener('keydown', function(event) {
+                if (event.key === 'Enter') {
+                    event.preventDefault();
+                    updateTask(label, editInput.value);
+                }
+            });
+
+            // Replace the label with the input field
+            label.textContent = '';
+            label.appendChild(editInput);
+            editInput.focus();
+        }
+
+        function updateTask(label, newText) {
+            // Remove the input field and update the label with the new text
+            var editInput = label.firstElementChild;
+            label.removeChild(editInput);
+            label.textContent = newText;
+
+            // Add the checkbox and delete button back to the label
+            var checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.addEventListener('change', function() {
+                if (this.checked) {
+                    label.parentElement.classList.add('completed');
+                } else {
+                    label.parentElement.classList.remove('completed');
+                }
+            });
+            label.insertBefore(checkbox, label.firstChild);
+
+            var deleteButton = document.createElement('span');
+            deleteButton.textContent = 'X';
+            deleteButton.className = 'deleteButton';
+            deleteButton.addEventListener('click', function(event) {
+                event.stopPropagation(); // Prevents triggering the parent label's click event
+                deleteTask(label.parentElement);
+            });
+            label.appendChild(deleteButton);
+        }
+
+        function showOptions(label) {
+            label.style.backgroundColor = '#f0f0f0';
+        }
+
+        function hideOptions(label) {
+            label.style.backgroundColor = 'transparent';
+        }
+
         function filterTasks(filter) {
             var taskItems = document.querySelectorAll('#taskDisplay li');
-        
-            taskItems.forEach(function(taskItem) {   // here we pass a parameter of taskItem and forEach function is used to iterate the items through list of tasks to check
-                if (filter === 'active' && !taskItem.classList.contains('completed')) {     //if task is active or not then it is filted based on these conditions.
+
+            taskItems.forEach(function(taskItem) {
+                if (filter === 'active' && !taskItem.classList.contains('completed')) {
                     taskItem.style.display = 'block';
                 } else if (filter === 'completed' && taskItem.classList.contains('completed')) {
                     taskItem.style.display = 'block';
@@ -57,30 +130,35 @@
                 }
             });
         }
-        
-        document.addEventListener('DOMContentLoaded', function() {  // function for taking the input from the input tag upon clivking the "enter key"
+
+        document.addEventListener('DOMContentLoaded', function() {
             var taskInput = document.getElementById('taskInput');
             var filterRadios = document.querySelectorAll('input[name="filter"]');
             var addButton = document.getElementById('addButton');
-     // new event listener is added to the button element in the JavaScript code using addEventListener,
-            // then event listener is triggered when the button is clicked, and it calls the addTask function.
-        
+
             taskInput.addEventListener('keydown', function(event) {
                 if (event.key === 'Enter') {
                     event.preventDefault();
                     addTask();
                 }
             });
-        
-            filterRadios.forEach(function(radio) {    // sets up event listeners on the filter radio buttons, when the user selects a different filter option,
-                radio.addEventListener('change', function() { // the event listener retrieves the selected value, triggers the filterTasks function to filter and display the tasks accordingly
+
+            filterRadios.forEach(function(radio) {
+                radio.addEventListener('change', function() {
                     var filter = this.value;
                     filterTasks(filter);
                 });
             });
-            // new event listener is added to the button element in the JavaScript code using addEventListener,
-            // then event listener is triggered when the button is clicked, and it calls the addTask function.
+
             addButton.addEventListener('click', function() {
                 addTask();
-              });
+            });
+
+            // Event delegation for editing tasks
+            var taskList = document.getElementById('taskDisplay');
+            taskList.addEventListener('click', function(event) {
+                if (event.target.tagName === 'LABEL') {
+                    editTask(event.target);
+                }
+            });
         });
